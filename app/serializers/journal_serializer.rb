415824eb227df49f5ac64fa73943713b2e7e667c
andrewinnocent <innocent.andrew@gmail.com>
2018-03-07 18:24:32 -0500

    Created One-to-Many Relationship
    Added has_many & belongs_to to files in this commit. Testing not working
    with error: {"user":["must exist"]}.

--- app/serializers/journal_serializer.rb
+++ app/serializers/journal_serializer.rb
@@ -1,3 +1,4 @@
 class JournalSerializer < ActiveModel::Serializer
   attributes :id, :state, :location_name, :rating, :comments, :date, :time
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307200902) do
+ActiveRecord::Schema.define(version: 20180307222908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20180307200902) do
     t.time "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_journals_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema.define(version: 20180307200902) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "journals", "users"
 end
